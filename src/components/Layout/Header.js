a6704b5ae752e79f97132155b25af02d3cec1630
s.feli <s.feli@DESKTOP-LOCAL>
2022-05-24 12:38:22 +0430

    add tailwind css

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,6 +1,12 @@
 const Header = () => (
   <header>
-    <div className="container">HEADER</div>
+    <div className="container">
+        HEADER
+    <div className="text-sm">
+        something
+    </div>
+    </div>
+
   </header>
 );
 export default Header;
