6f35aef4a894612f8794f041b7353d8b8073bc2d
s.feli <s.feli@DESKTOP-LOCAL>
2022-06-14 17:57:52 +0430

    add logo to header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,10 +1,9 @@
+import Logo from "../../assets/images/ganjoor-logo.svg";
 const Header = () => (
   <header>
     <div className="container">
-        HEADER
-    <div className="text-sm">
-        something
-    </div>
+        <img src={Logo} alt="گنجور" className="w-20 h-20 my-4 mx-auto" />
+        <hr className="border-neutral-400"/>
     </div>
 
   </header>
