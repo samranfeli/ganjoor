b7fd68c7b46b72235cf6f6563a4e0ed7c1b02825
s.feli <s.feli@DESKTOP-LOCAL>
2022-06-15 10:19:40 +0430

    add hover styles to poets items

--- src/components/Home/PoetItem.js
+++ src/components/Home/PoetItem.js
@@ -1,14 +1,14 @@
 import { Link } from "react-router-dom";
 const PoetItem = (props) => {
   return (
-    <Link to={props.item.fullUrl} className="block p-4 w-32 text-center">
+    <Link to={props.item.fullUrl} className="block p-4 w-32 text-center group">
       <div
-        className="avatar w-20 h-20 bg-cover mr-auto ml-auto bg-center rounded-full border border-slate-300"
+        className="avatar w-20 h-20 bg-cover mr-auto ml-auto bg-center rounded-full border border-slate-300 group-hover:scale-105 transition"
         style={{
           backgroundImage: `url("https://api.ganjoor.net${props.item.imageUrl}")`,
         }}
       />
-      <h3 className="mt-2 text-gray-600 text-sm">{props.item.name}</h3>
+      <h3 className="mt-2 text-gray-600 text-sm group-hover:text-[#2196f3] transition">{props.item.name}</h3>
     </Link>
   );
 };
