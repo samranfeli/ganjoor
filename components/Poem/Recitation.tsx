416ae2406ee4ac89db889bbc65b2a93fa679dd6d
Samran Feliimoghadam <s.feli@Itours.local>
2023-07-05 07:49:52 +0330

    add key to recitation map

--- components/Poem/Recitation.tsx
+++ components/Poem/Recitation.tsx
@@ -16,10 +16,10 @@ const Recitation : React.FC<Props> = props => {
     return(
         <div className='text-center'>
             <h4 className="mt-8 md:mt-10 mb-4 md:mb-6 text-lg font-semibold">خوانش ها</h4>
-            {recitations.map(recitation => <div className='mb-4 md:mb-8'>
+            {recitations.map(recitation => <div className='mb-4 md:mb-8' key={recitation.id}>
 
             
-            <audio controls className='w-full max-w-2xl mx-auto' preload="none" title='jkjkjkjkjkjk'>  
+            <audio controls className='w-full max-w-2xl mx-auto' preload="none" title={recitation.audioTitle}>  
                 <source src={recitation.mp3Url} type="audio/mpeg" title={`${recitation.audioTitle} به خوانش ${recitation.audioArtist}`} />  
                 مرورگر شما از پخش خوانش این شعر پشتیبانی نمیکند.
             </audio>  
