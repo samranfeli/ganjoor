3df6dee7ce7a1b4d03c2f5ad86444200768c942f
Samran Feliimoghadam <s.feli@Itours.local>
2023-07-05 13:38:28 +0330

    add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,8 +6,7 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    <link rel="icon" href="/favicon.ico" />
-                    
+                    <link rel="icon" href="/faviconGanjoor.ico" />
                     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css" />
                 </Head>
                 <body dir="rtl" className="md:text-lg">
