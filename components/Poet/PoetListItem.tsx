155a59c56a9f274429ccf4a883e63e0251ebc819
Samran Feliimoghadam <s.feli@Itours.local>
2023-07-05 13:39:17 +0330

    edit select poet loading effect in home page

--- components/Poet/PoetListItem.tsx
+++ components/Poet/PoetListItem.tsx
@@ -13,10 +13,7 @@ const PoetListItem:React.FC<Props> = props => {
     const {poet} = props;
     return(
         <Link href={poet.fullUrl!} className="group block shrink-0 grow-0 basis-20 m-3 text-center" onClick={() => {setClicked(true)}}>
-            <div className={`
-                relative ${clicked?"after:block after:rounded-full after:border-4 after:border-transparent after:border-t-orange-600 after:w-full after:h-full after:absolute after:top-0 after:left-0 after:animate-spin":""}
-                group-hover:scale-105 transition-all
-            `}>
+            <div className={`relative transition-all ${clicked?"scale-105":"group-hover:scale-105"}`}>
                 <Image
                     src={"https://api.ganjoor.net" + poet.imageUrl} 
                     alt={poet.name||""} 
@@ -24,9 +21,12 @@ const PoetListItem:React.FC<Props> = props => {
                     width={80} 
                     height={80} 
                 />
+                {clicked && <span
+                    className="block rounded-full border-4 border-transparent border-t-orange-600 absolute -top-2 -left-2 -bottom-2 -right-2 animate-spin transition-all"
+                />}
             </div>
             
-            <h3 className={`mt-2 group-hover:text-orange-400 transition-all leading-5 ${poet.id < 10 ? "font-bold text-md":"text-sm"}`}>{poet.name}</h3>
+            <h3 className={`mt-2 transition-all leading-5 ${poet.id < 10 ? "font-bold text-md":"text-sm"}`}>{poet.name}</h3>
 
         </Link>
     )
