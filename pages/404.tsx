803f81cfc4080f4e99792b4f147a448f4115500b
Samran Feliimoghadam <s.feli@Itours.local>
2023-07-14 23:16:59 +0330

    add custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,9 @@
+const Notfound: React.FC = () => {
+    return(
+        <div className="py-10 md:py-20 text-center">
+            <h3 className="mb-4 md:mb-6 text-xl md:text-3xl font-bold" >یافت می‌نشود ....</h3>
+            <p>صفحه یا شعر مورد نظر شما در این مجموعه وجود ندارد.</p>
+        </div>
+    )
+}
+export default Notfound;
