f966a9e540d5820ae2322f8a189f2d226a5f3471
Samran Feliimoghadam <s.feli@Itours.local>
2023-07-14 23:17:42 +0330

    add author meta tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,7 @@ class MyDocument extends Document {
                 <Head>
                     <link rel="icon" href="/faviconGanjoor.ico" />
                     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css" />
+                    <meta name="author" content="Samran Feli Moghadam" />
                 </Head>
                 <body dir="rtl" className="md:text-lg">
                     <Main />
