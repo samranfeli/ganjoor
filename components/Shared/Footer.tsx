1b40ccbdea8c475b5d14c4a92271694bea109dbb
Samran Feliimoghadam <s.feli@Itours.local>
2023-07-14 23:13:25 +0330

    add footer some links

--- components/Shared/Footer.tsx
+++ components/Shared/Footer.tsx
@@ -1,9 +1,17 @@
+import Link from "next/link";
+
 const Footer:React.FC = () => {
     return(
         <footer>
             <div className='mx-auto max-w-6xl'>
                 <div className="text-center py-6 border-t-2">
-                    گنجور
+                    <div>
+                        <Link className="mx-1 md:mx-3 text-gray-400 hover:text-gray-700 text-sm md:text-base" href="/"> خانه </Link>
+                        <Link className="mx-1 md:mx-3 text-gray-400 hover:text-gray-700 text-sm md:text-base" href="/about"> معرفی </Link>
+                        <Link className="mx-1 md:mx-3 text-gray-400 hover:text-gray-700 text-sm md:text-base" href="/random"> بیت تصادفی </Link>
+                        <Link className="mx-1 md:mx-3 text-gray-400 hover:text-gray-700 text-sm md:text-base" href="/privacy"> حریم خصوصی </Link>
+                        <Link className="mx-1 md:mx-3 text-gray-400 hover:text-gray-700 text-sm md:text-base" href="/contact"> تماس </Link>
+                    </div>
                 </div>
             </div>
         </footer>
