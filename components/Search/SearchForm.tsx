d8d5270c52a2dac0cd6c0348febabef48bdcdf39
Samran Feliimoghadam <s.feli@Itours.local>
2023-07-16 18:29:25 +0330

    improve search ux

--- components/Search/SearchForm.tsx
+++ components/Search/SearchForm.tsx
@@ -1,7 +1,7 @@
 import { Button, Form, Input, InputRef } from "antd";
 import { SearchOutlined } from '@ant-design/icons';
 import { useRouter } from 'next/router';
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 type Props = {
     placeholder?: string;
@@ -11,6 +11,8 @@ const SearchForm: React.FC<Props> = props => {
 
     const router = useRouter();
     const searchInputRef = useRef<InputRef>(null);
+
+    const [inputIsActive,setInputActive] = useState<boolean>(false);
 
     const [form] = Form.useForm();
 
@@ -23,12 +25,18 @@ const SearchForm: React.FC<Props> = props => {
         searchInputRef.current!.blur();
     }
 
+    const inputBlurHandle = () => {
+        if (!searchInputRef.current?.input?.value){
+            setInputActive(false);
+        }
+    } 
+
     return (
         <Form
             form={form}
             name="basic"
             onFinish={onSubmit}
-            className="relative"
+            className={`relative transition-all ${inputIsActive?"basis-72":"basis-48"}`}
             autoComplete="off"
         >
             <Form.Item className="mb-0" name="text">
@@ -36,6 +44,8 @@ const SearchForm: React.FC<Props> = props => {
                     className="w-full bg-white h-8 md:h-10 px-4 rounded placeholder:vazir md:text-base"
                     placeholder={props.placeholder || ""}
                     ref={searchInputRef}
+                    onFocus={() => {setInputActive(true)}}
+                    onBlur = {inputBlurHandle}
                 />
             </Form.Item>
 
